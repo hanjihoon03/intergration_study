28b472732a98c2ac12648b7720ed07b0ea4dc851
hanjihoon03 <gkswlgns7653@gmail.com>
2025-03-06 15:50:04 +0900

    first commit

---
+++ order/src/test/java/com/intergration/study/client/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intergration.study.client.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
